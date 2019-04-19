de661e31f44f497e8b09ca434544ab83dd421deb
amritofficial <itsamrit1996@gmail.com>
2019-04-19 17:04:53 -0400

    Create Get Data API Endpoint Route With Query Params

--- data_server/get_data.py
+++ data_server/get_data.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'this is a Flask'
+@app.route('/data/all', methods=['GET'])
+def get_data():
+    if request.args.get('format') == None:
+        return 'Route Works with JSON'
+
+    return 'route works ' + request.args.get('format')
 
 if __name__ == '__main__':
     app.run(debug=True)
