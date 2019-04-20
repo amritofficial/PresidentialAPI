328e04ad33617520daa9aebfd46ee32c593844b2
amritofficial <itsamrit1996@gmail.com>
2019-04-20 18:15:02 -0400

    Create Separate Process Data Class for Data Processing and Return CSV File

--- processing_server/process_data.py
+++ processing_server/process_data.py
@@ -1,4 +1,52 @@
+import csv, datetime, re
+from flask import send_file
+
 class Process_Data():
 
     def create_json_csv(self, json_data):
-        print("Hello")
+        custom_json = self.create_custom_json(json_data)
+
+        csv_file_name = 'results.csv'
+        csv_file = open(csv_file_name, 'w', newline='')
+        csv_file_writer = csv.writer(csv_file)
+
+        # create header
+        header_fields = ['Name', 'Party', 'Presidential term', 'President number', 'Ingestion Time']
+        csv_file_writer.writerow(header_fields)
+
+        for item in custom_json:
+            if 'Federalist' not in item['pp']:
+                row = [item['nm'],
+                       item['pp'],
+                       item['tm'],
+                       item['president'],
+                       datetime.datetime.now()]
+                csv_file_writer.writerow(row)
+
+        csv_file.close()
+        return send_file('results.csv', as_attachment=True)
+
+    def create_custom_json(self, json_data):
+        custom_json = json_data
+        for item in json_data:
+            item['nm'] = self.reverse_first_name(item['nm'])
+            item['pp'] = self.create_party_acroynm(item['pp'])
+            item['tm'] = self.create_term(item['tm'])
+
+        return sorted(custom_json, key=lambda d: d['nm'])
+
+    def reverse_first_name(self, full_name):
+        reversed_name = self.split_full_name(full_name)[::-1]
+        return reversed_name
+
+    def split_full_name(self, full_name):
+        return full_name.partition(' ')[0]
+
+    def create_party_acroynm(self, party):
+        split_words = re.findall(r"[\w']+", party)
+        acroynm = ''.join([word[0] for word in split_words])
+        return acroynm
+
+    def create_term(self, year):
+        years = year.split('-')
+        return years[0]
--- processing_server/request.py
+++ processing_server/request.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, jsonify
-from process_data import Process_Data
+from flask import Flask, request, jsonify, Response, send_from_directory, send_file
+import csv, datetime, re, json
+from processing_server.process_data import Process_Data
 import requests
 
 app = Flask(__name__)
@@ -9,20 +10,24 @@ url = 'http://127.0.0.1:5000/api/data'
 @app.route('/')
 @app.route('/data/all')
 def process_request():
+    process_data = Process_Data()
 
+    json_data = jsonify(get_json_data())
     query = request.args.get('format')
 
     if query == 'csv':
-        return get_json_data()
-    elif query == 'json' or query == None:
-        return "Hello World"
+       return process_data.create_json_csv(json_data)
+
+    elif query == 'json' or query is None:
+       return process_data.create_custom_json(json_data)
 
 def get_json_data():
     try:
         res = requests.get(url)
-        return jsonify(res.json())
+        return res.json()
     except:
         return "Error!"
 
-if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+
+
+
